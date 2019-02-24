1ab21548116bb791ace6978ddd9351176dc4d9c3
Jim Ritchey <jim0ritchey@gmail.com>
2019-02-23 22:49:46 -0500

    Updates

---
+++ precache-manifest.5e45d598020a0fb4217523b93b65863d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b73669ade2af22c21d45a58449213cf2",
+    "url": "/ClickyGame/static/media/gplaypattern.b73669ad.png"
+  },
+  {
+    "revision": "2c3a1b5a67c5d677a07d",
+    "url": "/ClickyGame/static/js/runtime~main.2c3a1b5a.js"
+  },
+  {
+    "revision": "ecefe99bd4bf86a742f5",
+    "url": "/ClickyGame/static/js/main.ecefe99b.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/ClickyGame/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "ecefe99bd4bf86a742f5",
+    "url": "/ClickyGame/static/css/main.038a5a29.chunk.css"
+  },
+  {
+    "revision": "814dd0736c3012f09e9168a17053facb",
+    "url": "/ClickyGame/index.html"
+  }
+];
